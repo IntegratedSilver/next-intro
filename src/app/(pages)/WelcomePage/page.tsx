038b93ed186887f7ef3cce326c991e0b908a3877
Axeolotl <a.d.l.c.rubio@gmail.com>
2024-09-11 19:23:58 -0700

    push

--- src/app/(pages)/WelcomePage/page.tsx
+++ src/app/(pages)/WelcomePage/page.tsx
@@ -1,9 +1,18 @@
+'use client'
+
+import WelcomeButton from '@/app/Components/WelcomeButton';
+import { Button } from '@nextui-org/react'
 import React from 'react'
 
 const WelcomePage = () => {
+  console.log('hey im here');
   return (
     <>
-        <div>Welcome Page</div>
+        <h1 className="text-center text-3xl mt-3">Welcome to the Blog</h1>
+        <p className="text-center mb-[20%]">click to see anything and everything</p>
+        <div className='flex justify-center'>
+           <WelcomeButton/>
+        </div>
     </>
   )
 }
